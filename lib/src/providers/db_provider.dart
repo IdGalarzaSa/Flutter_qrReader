ce361abcdbda863880c35f7dda618087f94a1867
Ivan Galarza <id.galarza.sa@gmail.com>
2021-03-10 01:36:09 -0400

    methods insert,update, delete and select data from database

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qr_reader/src/model/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -33,5 +34,52 @@ class DBProvider {
           )
         ''');
     });
+  }
+
+  Future<int> newScan(ScanModel newScan) async {
+    final db = await getDatabase;
+    final res = await db.insert('Scan', newScan.toJson());
+    return res;
+  }
+
+  Future<ScanModel> getScanById(int id) async {
+    final db = await getDatabase;
+    final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
+
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  Future<List<ScanModel>> getAllScans() async {
+    final db = await getDatabase;
+    final res = await db.query('Scans');
+
+    return res.isNotEmpty ? res.map((s) => ScanModel.fromJson(s)).toList() : [];
+  }
+
+  Future<List<ScanModel>> getScansByType(String type) async {
+    final db = await getDatabase;
+    final res = await db.query('Scans', where: 'tipo = ?', whereArgs: [type]);
+
+    return res.isNotEmpty ? res.map((s) => ScanModel.fromJson(s)).toList() : [];
+  }
+
+  Future<int> updateScan(ScanModel newScan) async {
+    final db = await getDatabase;
+    final res = await db.update('Scans', newScan.toJson(),
+        where: 'id = ?', whereArgs: [newScan.id]);
+    return res;
+  }
+
+  Future<int> deleteScan(ScanModel newScan) async {
+    final db = await getDatabase;
+    final res =
+        await db.delete('Scans', where: 'id = ?', whereArgs: [newScan.id]);
+    return res;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db = await getDatabase;
+    final res = await db.rawDelete('DELETE FROM Scans');
+    return res;
   }
 }
