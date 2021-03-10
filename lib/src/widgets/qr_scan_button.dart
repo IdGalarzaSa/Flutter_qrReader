493121904a0354c68b2bbbdda92d48053d2e5d46
Ivan Galarza <id.galarza.sa@gmail.com>
2021-03-09 21:32:58 -0400

    QR button functionality and QRScan model

--- lib/src/widgets/qr_scan_button.dart
+++ lib/src/widgets/qr_scan_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class QRScanButton extends StatelessWidget {
   const QRScanButton({Key key}) : super(key: key);
@@ -8,8 +9,18 @@ class QRScanButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      onPressed: () {},
       child: Icon(Icons.qr_code),
+      onPressed: () async {
+        // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        //   "#3D8BEF", // Color hexadecimal, que es el color de la línea en la superposición del código de barras.
+        //   "Cancel", // Texto del botón cancelar en la pantalla
+        //   true, // Valor bool utilizado para mostrar u ocultar el icono de flash
+        //   ScanMode.QR, // Tipo de escaneo
+        // );
+
+        String barcodeScanRes = "https://www.google.com.bo";
+        print(barcodeScanRes);
+      },
     );
   }
 }
